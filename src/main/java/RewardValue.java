d241a3a9911a223a6fbd8cbee5e609ff4775c004
[sz1978] <[shuo.zhong@live.de]>
2024-06-18 21:04:44 +0200

    task1

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,40 @@
+public class RewardValue {
+
+    private double cashValue;
+
+    private int milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+    }
+
+    public void setCashValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public void setMilesValue(int milesValue) {
+        this.milesValue = milesValue;
+    }
+
+    public int getMilesValue() {
+        return milesValue;
+    }
+
+    public int convertFromCashToMiles(double cashValue) {
+        milesValue = (int) cashValue;
+        return milesValue;
+    }
+
+    public double convertFromMilesToCash(int milesValue) {
+        cashValue = (double) milesValue * 0.0035;
+        return cashValue;
+    }
+}
